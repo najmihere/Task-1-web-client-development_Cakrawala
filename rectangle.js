ab9c8ba1ff63004954346768bea0b4817b22b82d
najmihere <najmihilal555@gmail.com>
2025-03-08 22:04:18 +0700

    Update rectangle.js

---
+++ rectangle.js
@@ -0,0 +1,8 @@
+function calculateRectangleArea(length, width) {
+    return length * width;
+}
+
+const length = 5;
+const width = 3;
+const area = calculateRectangleArea(length, width);
+console.log(`Luas persegi panjang: ${area}`);
